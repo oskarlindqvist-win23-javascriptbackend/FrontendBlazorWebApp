3edf0d412ea161a2ca85ec3da14fc66ced6ab6ea
oskar lindqvist <lindqvist_11@hotmail.com>
2024-05-23 00:32:45 +0700

    Account details done.

---
+++ FrontendBlazorWebApp/FrontendBlazorWebApp.Client/Models/CourseCardModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FrontendBlazorWebApp.Client.Models;
+
+public class CourseCardModel
+{
+    public string? Id { get; set; }
+    public string? ImageUri { get; set; }
+    public bool IsBestseller { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public decimal StarRating { get; set; }
+    public string? Reviews { get; set; }
+    public string? LikesInProcent { get; set; }
+    public string? Likes { get; set; }
+    public string? Hours { get; set; }
+}
